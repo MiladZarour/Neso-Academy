ffcfca5843578c0d11a94ab68f4204cda6656f46
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-13 21:34:49 +0100

    Inserting Node At the Beginning of the Single List

---
+++ IntroToLinkedList/7InsertingNodeAtTheBeginningOfTheList/1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *add_beg(struct node *head, int d)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = d;
+    ptr->link = NULL;
+
+    ptr->link = head;
+    head = ptr;
+    return head;
+}
+
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = 98;
+    ptr->link = NULL;
+
+    head->link = ptr;
+
+    int data = 3;
+
+    head = add_beg(head, data);
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+}
---
+++ IntroToLinkedList/7InsertingNodeAtTheBeginningOfTheList/2Possible Mistake/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void add_beg(struct node **head, int d)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = d;
+    ptr->link = NULL;
+
+    ptr->link = *head;
+    *head = ptr;
+}
+
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = 98;
+    ptr->link = NULL;
+
+    head->link = ptr;
+
+    int data = 3;
+
+    add_beg(&head, data);
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+}
