c875331ae0b0a67de678b6c89b011e65c0a8312a
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2021-11-13 22:04:18 +0100

    Added Insertion At A Certain Position Single Linked List

---
+++ IntroToLinkedList/8InsertingNodeAtACertainPosition/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void add_at_end(struct node *head, int data)
+{
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    temp->data = data;
+    temp->link = NULL;
+
+    while (ptr->link != NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+}
+
+void add_beg(struct node **head, int d)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = d;
+    ptr->link = NULL;
+
+    ptr->link = *head;
+    *head = ptr;
+}
+
+void add_at_pos(struct node *head, int data, int pos)
+{
+    struct node *ptr = head;
+    struct node *ptr2 = (struct node *)malloc(sizeof(struct node));
+    ptr2->data = data;
+    ptr2->link = NULL;
+
+    pos--;
+    while (pos != 1)
+    {
+        ptr = ptr->link;
+        pos--;
+    }
+    ptr2->link = ptr->link;
+    ptr->link = ptr2;
+}
+
+int main()
+{
+    struct node *head = (struct node *)malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    add_at_end(head, 98);
+    add_at_end(head, 3);
+
+    int data = 67, position = 3;
+
+    add_at_pos(head, data, position);
+    struct node *ptr = head;
+
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    return 0;
+}
